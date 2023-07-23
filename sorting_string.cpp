92f70efa340b14650726a03afca174f27d7d4020
Harsh <harshvaghasiya21@gmail.com>
2023-07-23 22:19:09 +0530

    Sorting of string

---
+++ sorting_string.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s="helloharshal";
+    sort(s.begin(), s.end());
+    cout << s << endl;
+    return 0;
+}
