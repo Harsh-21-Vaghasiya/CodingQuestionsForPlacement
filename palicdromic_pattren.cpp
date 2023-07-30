10fd8ee493740fa6430b55ea6b7d0ddd5c91ac61
Harsh <harshvaghasiya21@gmail.com>
2023-07-30 10:58:59 +0530

    Palicdromic_pattren

---
+++ palicdromic_pattren.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+     for (int i = 0; i < 5; i++)
+    {
+        for (int j = i; j < 5; j++)
+        {
+            cout << " ";
+        }
+        for (int z = i; z >0; z--)
+        {
+            cout <<z ;
+        }
+        for (int z = 1; z <=i; z++)
+        {
+            cout <<z ;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
