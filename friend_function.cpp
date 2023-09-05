00bf18a21b3cb71e50eea64caec8d8cdd733d444
Harsh <harshvaghasiya21@gmail.com>
2023-09-05 17:16:34 +0530

    friend fnction added

---
+++ friend_function.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+class Complex
+{
+    int a;
+    int b;
+
+public:
+    void set(int a, int b)
+    {
+        this->a = a;
+        this->b = b;
+    }
+
+    friend Complex sum(Complex a, Complex b);
+    void prints()
+    {
+        cout << "Number of the objects is " << this->a << " + " << this->b << "i" << endl;
+    }
+};
+
+Complex sum(Complex o1, Complex o2)
+{
+    Complex sum;
+    sum.set(o1.a + o2.a, o1.b + o2.b);
+    return sum;
+}
+int main()
+
+{
+
+    Complex complex, complex2, sums;
+
+    complex.set(1, 2);
+    complex2.set(3, 4);
+    complex.prints();
+
+    sums = sum(complex, complex2);
+    sums.prints();
+}
